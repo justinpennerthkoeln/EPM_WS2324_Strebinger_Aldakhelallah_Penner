1b3d2e8388ab7b7d07cd4d16146e6fd75c35b1c2
justin-penner <102801880+justin-penner@users.noreply.github.com>
2023-12-17 22:11:05 +0100

    Adding Routes and giving back important information

--- Rapid Prototype/db/create.sql
+++ Rapid Prototype/db/create.sql
@@ -1,5 +1,50 @@
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS connections;
+DROP TABLE IF EXISTS alerts;
+DROP TABLE IF EXISTS collections;
+DROP TABLE IF EXISTS tasks;
+
 CREATE TABLE users (
     username varchar(255) NOT NULL,
     password varchar(255) NOT NULL
+);
+
+CREATE TABLE collections (
+    id int NOT NULL,
+    uuid uuid DEFAULT gen_random_uuid(),
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+
+CREATE TABLE connections (
+    id int NOT NULL,
+    collection_id int NOT NULL,
+    platform_id int NOT NULL,
+    platform_key varchar(255) NOT NULL,
+    user_id int NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (collection_id) REFERENCES collections(id)
+);
+
+CREATE TABLE alerts (
+    id int NOT NULL,
+    collection_id int NOT NULL,
+    name varchar(255) NOT NULL,
+    description varchar(255) NOT NULL,
+    platform_id int NOT NULL,
+    timestamp timestamp NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (collection_id) REFERENCES collections(id)
+);
+
+CREATE TABLE tasks (
+    id int NOT NULL,
+    collection_id int NOT NULL,
+    status int NOT NULL,
+    name varchar(255) NOT NULL,
+    description varchar(250) NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (collection_id) REFERENCES collections(id)
 );
--- Rapid Prototype/server.js
+++ Rapid Prototype/server.js
@@ -11,10 +11,54 @@ const SERVER = HTTP.createServer(APP);
 //Middleware
 APP.use(EXPRESS.static(__dirname + '/static'));
 DOTENV.config();
+APP.set("view engine", "ejs");
+
+//modules
+const COLLECTIONSMODEL = require('./models/collectionsModel.js');
+const CONNECTIONSMODEL = require('./models/connectionsModel.js');
+const ALERTSMODEL = require('./models/alertsModel.js');
+const TASKSMODEL = require('./models/tasksModel.js');
 
 //Routing
 APP.get("/", (req, res) => {
-    res.send({"message": "Hello World!"});
+    res.send({"msg" : "Hello World!"});
+});
+
+APP.get("/login", (req, res) => {
+
+});
+
+APP.get("/register", (req, res) => {
+
+});
+
+APP.get("/:uuid", async (req, res) => {
+    const COLLECTION = await (await COLLECTIONSMODEL.getCollection(req.params.uuid)).rows[0];
+    const CONNECTIONS = await (await CONNECTIONSMODEL.getConnectionsFromCollectionId(await COLLECTION.id)).rows;
+
+    res.send(CONNECTIONS);
+});
+
+APP.get("/:uuid/alerts", async (req, res) => {
+    const COLLECTION = await (await COLLECTIONSMODEL.getCollection(req.params.uuid)).rows[0];
+    const ALERTS = await (await ALERTSMODEL.getAlerts(await COLLECTION.id)).rows;
+
+    res.send(ALERTS);
+});
+
+APP.get("/:uuid/tasks", async (req, res) => {
+    const COLLECTION = await (await COLLECTIONSMODEL.getCollection(req.params.uuid)).rows[0];
+    const TASKS = await (await TASKSMODEL.getTasksByCollectionId(await COLLECTION.id)).rows;
+
+    res.send(TASKS);
+});
+
+APP.get("/:uuid/inspection", async (req, res) => {
+
+});
+
+APP.get("/:uuid/settings", async (req, res) => {
+
 });
 
 const IO = new SOCKETIO.Server(SERVER);
