edebc05cfbe36d5ff2dbabb6ecb4b04fafea25ea
justin-penner <justin.penner@inspired.consulting>
2024-01-02 01:15:23 +0100

    short style of settings page

--- Rapid Prototype/static/js/settings.js
+++ Rapid Prototype/static/js/settings.js
@@ -6,19 +6,35 @@ const SOCKET = io('ws://localhost:80');
 SOCKET.on('connect', () => {
     console.log(`Connected with ${SOCKET.id}.`);
 
-    const h1 = document.querySelector('h1');
     const forms = document.querySelectorAll('form');
     const platformForm = document.querySelectorAll('#add-platform-form')[0];
     const sideNav = document.querySelectorAll('#aside-nav ul a');
 
     SOCKET.emit('get-details', (window.location.pathname.split('/')[1]));
+    SOCKET.on('got-details', (details) => {
+        const HEADER = document.querySelector('main > header');
+        const DATEOPTIONS = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric'
+        };
+        const DATE = new Date(details.timestamp).toLocaleString('de-DE', DATEOPTIONS);
+        const MEMBERS = details.members.map((member) => {
+            return `@${member.username}`;
+        }).join(', ');
 
-    SOCKET.on('got-details', (collection) => {
-        h1.innerHTML = collection.name;
+        document.title = `SynergyHub | ${details.name}`;
 
-        forms.forEach((element) => {
-            element.action = '/' + collection.uuid + '/' + element.action.split('/')[3];
-        });
+        HEADER.children[0].textContent = details.name;
+        HEADER.children[1].textContent = `${DATE} — ${MEMBERS}`;
+        HEADER.children[2].textContent = details.description;
+    });
+
+    SOCKET.emit('get-member', {userId: localStorage.getItem('userId'), uuid: window.location.pathname.split('/')[1]});
+
+    SOCKET.on('got-member', (member) => {
+        localStorage.setItem('username', member.username)
+        localStorage.setItem('membershipId', member.membership_id);
     });
 
     platformForm.addEventListener('submit', ($event) => {
